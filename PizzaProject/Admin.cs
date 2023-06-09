325d668beb367620aee70cd9045b4f68a346f105
DESKTOP-4NIHT4U\Acer <saloroman23@gmail.com>
2023-06-09 21:53:32 +0300

    Set up structure for administration part

---
+++ PizzaProject/Admin.cs
@@ -0,0 +1,54 @@
+﻿using PizzaProject.Stubs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzaProject
+{
+    internal class Admin
+    {
+        private PizzeriaData _pizzeriaData;
+
+        public Admin(PizzeriaData pizzeriaData)
+        {
+            _pizzeriaData = pizzeriaData;
+        }
+
+        public string GetStaffInfo(IStaff staff)
+        {
+            return "";
+        }
+
+        public void SetVipStatus(Customer customer, HashSet<VipLvl> status)
+        {
+
+        }
+
+        public void AddStaff(IStaff staff)
+        {
+
+        }
+
+        public void RemoveStaff(IStaff staff)
+        {
+
+        }
+
+        public void AddICashRegister(ICashRegister cashRegister)
+        {
+
+        }
+
+        public void RemoveICashRegister(ICashRegister cashRegister)
+        {
+
+        }
+
+        public void SetManager(IPerson person)
+        {
+
+        }
+    }
+}
---
+++ PizzaProject/PizzeriaData.cs
@@ -0,0 +1,68 @@
+﻿using PizzaProject.Stubs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzaProject
+{
+    internal class PizzeriaData
+    {
+        private Manager _manager;
+        private HashSet<IStaff> _staff;
+        private ChefManager _chefManager;
+        private LoyaltyProgram _loyaltyProgram;
+        private Menu _menu;
+        private HashSet<ICashRegister> _cashRegs;
+        private Storage _productStorage;
+
+        public PizzeriaData(object[] args)
+        {
+
+        }
+
+        public Manager Manager { get => _manager; }
+        public IEnumerable<IStaff> Staff { get => _staff; }
+        public ChefManager ChefManager { get => _chefManager; }
+        public LoyaltyProgram LoyaltyProgram { get => _loyaltyProgram; }
+        public Menu Menu { get => _menu; }
+        public IEnumerable<ICashRegister> CashRegs { get => _cashRegs; }
+        public Storage ProductStorage { get => _productStorage; }
+
+        public string GetStaffInfo(IStaff staff)
+        {
+            return "";
+        }
+
+        public void SetVipStatus(Customer customer, HashSet<VipLvl> status)
+        {
+
+        }
+
+        public void AddStaff(IStaff staff)
+        {
+
+        }
+
+        public void RemoveStaff(IStaff staff)
+        {
+
+        }
+
+        public void AddICashRegister(ICashRegister cashRegister)
+        {
+
+        }
+
+        public void RemoveICashRegister(ICashRegister cashRegister)
+        {
+
+        }
+
+        public void SetManager(IPerson person)
+        {
+
+        }
+    }
+}
