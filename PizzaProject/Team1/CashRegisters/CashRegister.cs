034fd3e4e51286c7babe8471d5e8b2a8075c1283
Ostap <ostapilienkiv@gmail.com>
2023-06-09 17:52:41 +0300

    simple CashRegister realisation

---
+++ PizzaProject/Team1/CashRegisters/CashRegister.cs
@@ -0,0 +1,58 @@
+﻿using PizzaProject.Team1.TemporaryClasses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzaProject.Team1.CashRegisters
+{
+    internal class CashRegister : ICashRegister
+    {
+        private IMenu _menu;
+        public IMenu GetMenu { get => _menu; }
+
+        HashSet<Wallet> _localWallets;
+        public HashSet<Wallet.MoneyCategory> PaymentMethod { get => _localWallets.Select(x => x.Category).ToHashSet(); }
+
+        List<object> _customersInQueue;
+        public int CustomersInQueue { get => _customersInQueue.Count; }
+
+
+
+        public event OrderApperiance NewOrderApperiance;
+
+
+
+        public HashSet<object> CheckCustomerStatus(object customer)
+        {
+            throw new NotImplementedException();
+        }
+        public void AddToQueue(object customer)
+        {
+            _customersInQueue.Add(customer);
+        }
+        public void DeleteFromQueue(object customer)
+        {
+            _customersInQueue.Remove(customer);
+        }
+        public bool Pay(Wallet customerWallet, object order)
+        {
+            /*
+             
+                if(_localWallets.Any(lw => lw.Category == customerWallet.Category))
+                {
+                    if(customerWallet.MoneyCount >= order.GetTotalPrice())
+                    {
+                        customerWallet.Pay(order.GetTotalPrice(),_localWallets.First(lw => lw.Category == customerWallet.Category));
+                        NewOrderApperiance(order);
+                        return true;
+                    }
+                }
+                return false;
+
+             */
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ PizzaProject/Team1/CashRegisters/ICashRegister.cs
@@ -0,0 +1,24 @@
+﻿using PizzaProject.Team1.TemporaryClasses;
+namespace PizzaProject.Team1.CashRegisters
+{
+    public delegate void OrderApperiance(object order);
+    internal interface ICashRegister
+    {
+        IMenu GetMenu { get; }
+        HashSet<Wallet.MoneyCategory> PaymentMethod { get; }
+        int CustomersInQueue { get; }
+
+        /// <summary>
+        /// Оскільки нам потрібно щоб після оплати замовлення передавалось на кухню, 
+        /// я подумав що було б добре щоб покупець передавав в параметрі саме замовлення.
+        /// Тоді я  у методі оплати знімаю з рахунку покупця стільки грошех, скільки буде вказано в замовленні,
+        /// заодно зможу спокійно викликати івент
+        /// </summary>
+        bool Pay(Wallet customerWallet, object order);
+        HashSet<object> CheckCustomerStatus(object customer);
+        void AddToQueue(object customer);
+        void DeleteFromQueue(object customer);
+
+        event OrderApperiance NewOrderApperiance;
+    }
+}
