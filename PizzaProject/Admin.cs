bf9731f80f0cb58da20bc6b5ea3e12f2a2eb1f87
DESKTOP-4NIHT4U\Acer <saloroman23@gmail.com>
2023-06-10 13:10:45 +0300

    Add PizzeriaData method calls in Admin

--- PizzaProject/Admin.cs
+++ PizzaProject/Admin.cs
@@ -18,37 +18,37 @@ namespace PizzaProject
 
         public string GetStaffInfo(IStaff staff)
         {
-            return "";
+            return _pizzeriaData.GetStaffInfo(staff);
         }
 
         public void SetVipStatus(Customer customer, HashSet<VipLvl> status)
         {
-
+            _pizzeriaData.SetVipStatus(customer, status);
         }
 
         public void AddStaff(IStaff staff)
         {
-
+            _pizzeriaData.AddStaff(staff);
         }
 
         public void RemoveStaff(IStaff staff)
         {
-
+            _pizzeriaData.RemoveStaff(staff);
         }
 
         public void AddICashRegister(ICashRegister cashRegister)
         {
-
+            _pizzeriaData.AddICashRegister(cashRegister);
         }
 
         public void RemoveICashRegister(ICashRegister cashRegister)
         {
-
+            _pizzeriaData.RemoveICashRegister(cashRegister);
         }
 
         public void SetManager(IPerson person)
         {
-
+            _pizzeriaData.SetManager(person);
         }
     }
 }
