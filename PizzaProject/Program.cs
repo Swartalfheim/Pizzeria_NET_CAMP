5d077cf2a756eed8ae7b3e098ff6189162761672
pepe <106858735+PeEpEGa@users.noreply.github.com>
2023-06-16 21:02:38 +0300

    commit

--- PizzaProject/Program.cs
+++ PizzaProject/Program.cs
@@ -1,24 +1,9 @@
-﻿using PizzeriaTask_my_part_;
-namespace PizzaProject
+﻿namespace PizzaProject
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            var customerGenerator = new CustomerGenerator(new DefaultCustomerGenerator(10));
-            //var CustomerGenerator = new CustomerGenerator(new RandomCustomerGenerator(0, 15));
-            var users = customerGenerator.GenerateVisitorsForSimulation();
-
-            foreach (var user in users)
-            {
-                Console.WriteLine(user.Name);
-                foreach (var lvl in user.VipLvls)
-                {
-                    System.Console.WriteLine(lvl);
-                }
-                System.Console.WriteLine();
-            }
-
             Console.WriteLine("Hello, World!");
         }
     }
